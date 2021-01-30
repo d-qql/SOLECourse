4985cfce708576688e807cf814223d9a083ed5ad
d-qql <petrov.dmitrii@phystech.edu>
2021-01-31 02:29:54 +0300

    vector overloads added

--- utility/Overloads.h
+++ utility/Overloads.h
@@ -7,6 +7,28 @@
 #include <vector>
 
 template<typename T>
+std::vector<T> operator*(const T &k, std::vector<T> const &vec){
+    std::vector<T> result;
+    result.resize(vec.size());
+    for(size_t i = 0; i<result.size(); ++i){
+        result[i] = k*vec[i];
+    }
+    return result;
+}
+
+template<typename T>
+std::vector<T> operator+(const std::vector<T>& fVec, const std::vector<T>& sVec){
+    std::vector<T> res;
+    if(fVec.size() == sVec.size()){
+        res.resize(fVec.size());
+        for(size_t i = 0; i < fVec.size(); ++i){
+            res[i]=fVec[i]+sVec[i];
+        }
+    }
+    return res;
+}
+
+template<typename T>
 std::vector<T> operator-(const std::vector<T>& fVec, const std::vector<T>& sVec){
     std::vector<T> res;
     if(fVec.size() == sVec.size()){
@@ -27,5 +49,7 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& b){
     os<<")\n";
     return os;
 }
+
+
 
 #endif //SOLECOURSE_OVERLOADS_H
