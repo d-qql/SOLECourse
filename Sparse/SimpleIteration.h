7f241d9ab8dd48cc86f456bccc378b133ba81a3a
d-qql <petrov.dmitrii@phystech.edu>
2021-01-31 02:28:32 +0300

    SimpleIteration Impl

---
+++ Sparse/SimpleIteration.h
@@ -0,0 +1,44 @@
+//
+// Created by d-qql on 30.01.2021.
+//
+
+#ifndef SOLECOURSE_SIMPLEITERATION_H
+#define SOLECOURSE_SIMPLEITERATION_H
+#include "CSR.h"
+#include "../utility/Consts.h"
+#include "../utility/gnuplot-iostream.h"
+template<typename T>
+std::vector<T> SimpleIteration(const CSR<T> &A, const std::vector<T> &b, const T &tao){
+    using elm_t = typename CSR<T>::elm_t;
+
+    std::vector<T> x(A.sizeH());
+    std::vector<T> r = A * x - b;
+    int i = 0;
+    T N = norm(r);
+    std::vector<std::pair<int, double>> plotData;
+    while (N > tolerance<T>) {
+        x = -tao * (A * x - b) + x;
+        r = A * x - b;
+        plotData.emplace_back(i, log(N));
+        ++i;
+        N = norm(r);
+        std::cout<<N<<std::endl;
+    }
+
+    std::ofstream fout("../PlotData/SimpleIteration/SimpleIteration_Speed" + std::to_string(tao) + ".txt");
+    for(auto k: plotData){
+        fout<<k.first<<" "<<k.second<<"\n";
+    }
+    fout.close();
+
+    Gnuplot gp;
+    gp<<"set xlabel 'Номер итерации' \n"
+        "set ylabel 'ln(|r|))'\n"
+        "set grid\n"
+        "set title 'Зависимость логарифма модуля невязки от номера итерации ПМИ' font 'Helvetica Bold, 10'\n";
+    gp << "plot '-' with lines title 'Iter->ln(|r|)' lc rgb 'blue'\n";
+    gp.send1d(plotData);
+    return x;
+}
+
+#endif //SOLECOURSE_SIMPLEITERATION_H
