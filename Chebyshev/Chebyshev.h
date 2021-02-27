22a934b749b71874c2b1836c599e7538ef7a4e58
d-qql <petrov.dmitrii@phystech.edu>
2021-02-27 18:25:53 +0300

    Coefs calculation

--- Chebyshev/Chebyshev.h
+++ Chebyshev/Chebyshev.h
@@ -6,6 +6,20 @@
 #define SOLECOURSE_CHEBYSHEV_H
 #include "vector"
 #include <ctgmath>
+#include "../utility/Consts.h"
+
+template<typename T>
+unsigned long long int C(int n, int k){
+    unsigned long long int res = 1;
+    for(int i = k + 1; i <= n; ++i ){
+        res *= i;
+    }
+    for(int i = 1; i <= n-k; ++i){
+        res /= i;
+    }
+    return res;
+}
+
 template<typename T>
 std::vector<T> ChebyshevRoots(std::pair<T, T> section, size_t PowOf2){
     size_t PolyOrder = std::pow(2, PowOf2);
@@ -37,4 +51,41 @@ std::vector<T> ChebyshevRoots(std::pair<T, T> section, size_t PowOf2){
     return res;
 }
 
+template<typename T>
+std::vector<T> ChebyshevCoef(std::pair<T, T> sec, size_t PolyOrder){
+    std::vector<T> coefs_f, coefs_s, coefs_new;
+
+    coefs_f.emplace_back(static_cast<T>(1));
+
+    coefs_s.emplace_back( static_cast<T>(0));
+    coefs_s.emplace_back( static_cast<T>(1));
+
+    for(size_t k = 1; k < PolyOrder; ++k){ //порядок текущего полинома, после прохода цикла получим полином порядком выше
+        coefs_new.resize(k+2);  //к+2 = число кэфов в полиноме порядка к+1
+        for(size_t i = 0; i < k+1; ++i){
+            coefs_new[i] -= coefs_f[i];
+            coefs_new[i+1] += 2 * coefs_s[i];
+            if(Tabs(coefs_new[i]) < tolerance<T>) coefs_new[i] = static_cast<T>(0);
+            if(Tabs(coefs_new[i+1]) < tolerance<T>) coefs_new[i+1] = static_cast<T>(0);
+        }
+        coefs_f = coefs_s;
+        coefs_s = coefs_new;
+        coefs_new.clear();
+    }
+    coefs_new.resize(PolyOrder+1);
+    for(size_t n = 0; n <= PolyOrder; ++n){
+        for(size_t k = 0; k <= n; ++k){
+            coefs_new[n-k] += coefs_s[n] * C<T>(n, k)*pow(2./(sec.second-sec.first), n-k)*pow(-(sec.second+sec.first)/(sec.second-sec.first), k);
+        }
+    }
+    for(size_t i = 0; i <=PolyOrder; ++i){
+        coefs_new[i]/=coefs_new[PolyOrder];
+    }
+    std::cout<<coefs_new;
+    std::cout<<coefs_s;
+    for(size_t i = 0; i <= PolyOrder; ++i){
+        std::cout<<coefs_new[i]<<"*x^"<<i<<"+";
+    }
+    return coefs_new;
+}
 #endif //SOLECOURSE_CHEBYSHEV_H
