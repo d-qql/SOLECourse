824d62fe74609edf767ceefe1dfe206e66c4866f
d-qql <petrov.dmitrii@phystech.edu>
2021-04-20 18:17:06 +0300

    BiCG

---
+++ Sparse/BiCG.h
@@ -0,0 +1,55 @@
+//
+// Created by d-qql on 12.04.2021.
+//
+
+#ifndef SOLECOURSE_BICG_H
+#define SOLECOURSE_BICG_H
+#include <vector>
+#include "CSR.h"
+
+template<typename T>
+std::vector<T> BiCG(const CSR<T>& A, const std::vector<T>& b){
+    std::vector<T> x(b.size());
+    std::vector<T> r0 = b - A * x;
+    std::vector<T> r1 = r0;
+    std::vector<T> p0, p1;
+    std::vector<T> z0, z1;
+    CSR<T> At = A.transpose();
+    T N = norm(r0);
+    T rho;
+    T prev_rho;
+    T Beta;
+    T Alfa;
+    long long int i = 1;
+    while( N > tolerance<T> ){
+        rho = r1*r0;
+        try {
+            if (Tabs(rho) < tolerance<T>) throw 1;
+        }
+        catch(int){
+            std::cout<<"FAIL!";
+            std::cout<<"\n"<<i<<"\n";
+            i = 1;
+           // return BiCG(A, b, x);
+        }
+        if( i == 1 ){
+            p0 = r0;
+            p1 = r1;
+        }else{
+            Beta = rho/prev_rho;
+            p0 = r0 + Beta * p0;
+            p1 = r1 + Beta * p1;
+        }
+        z0 = A * p0;
+        z1 = At * p1;
+        Alfa = rho/(p1*z0);
+        x = x + Alfa * p0;
+        r0 = r0 - Alfa * z0;
+        r1 = r1 - Alfa * z1;
+        N = norm(r0);
+        ++i;
+        prev_rho = rho;
+    }
+    return x;
+}
+#endif //SOLECOURSE_BICG_H
--- main.cpp
+++ main.cpp
@@ -27,6 +27,7 @@
 #include "Sparse/CG.h"
 #include "Givens/Rotations.h"
 #include "GMRES/GMRES.h"
+#include "Sparse/BiCG.h"
 int main() {
     std::set<Triplet<double>> in;
    /* size_t p, r;
@@ -39,7 +40,7 @@ int main() {
     }
     std::cout<<in.size();
 */
-    size_t SZ = 50;
+    size_t SZ = 5;
 
     std::vector<double> b;
 
@@ -48,7 +49,10 @@ int main() {
 
 
         CSR<double> A = CSR<double>(SZ, SZ, in);
-        std::cout<<GMRES(A, b)<<GaussSeidel(A, b);
+        std::vector<double> x = GenerateVector<double>(SZ, -1, 1);
+        std::cout<<GMRES(A, b, 10)<<GaussSeidel(A, b)<<BiCG(A,b)<<CGmethod(A, b);
+        A.print();
+        A.transpose().print();
     Gnuplot gp;
     gp<<"set xlabel 'Номер итерации' \n"
         "set ylabel 'Логарифм нормы невязки'\n"
